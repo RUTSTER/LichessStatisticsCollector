3d713752534ef9e036956126a47a19655e504889
RUTSTER <Maks.Andrugin@gmail.com>
2020-07-01 17:12:05 +0300

    [General] Add possibility to print chess statistics
    Format: 'gamesNumber:rating'

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,10 @@
-public class Application {
-    public static void main(String[] args) {
+import java.io.IOException;
 
+public class Application {
+    public static void main(String[] args) throws IOException {
+        String personalAccessToken = "GOytCybFqf96znOQ";
+        String statistics = (new APIWorker()).getStatistics(personalAccessToken);
+        String result = (new JSONWorker()).parseJSON(statistics);
+        System.out.println(result);
     }
 }
